eb16b6e62573d7036ba41b4a861f0d7acc44f2ec
mmochan <mmochan@mac.com>
2014-02-24 21:51:04 +1000

    first cookbook

--- jython/recipes/default.rb
+++ jython/recipes/default.rb
@@ -1,72 +1,14 @@
-package "build-essential" do
-  action :install
-end
 
-user node[:redis][:user] do
-  action :create
-  system true
-  shell "/bin/false"
-end
-
-directory node[:redis][:dir] do
-  owner "root"
-  mode "0755"
-  action :create
-end
-
-directory node[:redis][:data_dir] do
-  owner "redis"
-  mode "0755"
-  action :create
-end
-
-directory node[:redis][:log_dir] do
-  mode 0755
-  owner node[:redis][:user]
-  action :create
-end
 
 remote_file "#{Chef::Config[:file_cache_path]}/jython-standalone-2.7-b1.jar" do
-  #source "http://search.maven.org/remotecontent?filepath=org/python/jython-standalone/2.7-b1/jython-standalone-2.7-b1.jar"
   source "http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-b1/jython-installer-2.7-b1.jar"
   action :create_if_missing
 end
 
-bash "compile_redis_source" do
+bash "install_jython" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-    tar zxf redis-2.8.6.tar.gz
-    cd redis-2.8.6
-    puts "Compiling"
-    make && make install
+    java -jar jython-installer-2.7-b1.jar
   EOH
-  creates "/usr/local/bin/redis-server"
-end
-
-service "redis" do
-  provider Chef::Provider::Service::Upstart
-  subscribes :restart, resources(:bash => "compile_redis_source")
-  supports :restart => true, :start => true, :stop => true
-end
-
-template "redis.conf" do
-  path "#{node[:redis][:dir]}/redis.conf"
-  source "redis.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "redis")
-end
-
-template "redis.upstart.conf" do
-  path "/etc/init/redis.conf"
-  source "redis.upstart.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "redis")
-end
-
-service "redis" do
-  action [:enable, :start]
+  creates "/usr/local/bin/jython"
 end
